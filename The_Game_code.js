933548c38bdf6c71c45d8490a96d6a86fd685884
Madhu1149 <23477mt@hvhs.school.nz>
2025-03-16 23:03:44 +0000

    creating obsticles and fixing errors

--- The_Game_code.js
+++ The_Game_code.js
@@ -11,20 +11,16 @@ function setup() {
 	console.log("");
     cnv = new Canvas(windowWidth, windowHeight);
     world.gravity.y = 20;
-    Player_Box = new Sprite(300,620, 50,50, 'd');
+    Player_Box = new Sprite(300,620, 50,50, '');
     Player_Box.color = '#00bcd4';
     Player_Box.friction   = 0;
 
     //Platform
-    let Platform = new Sprite(900, 750, 2008, 10, 's');
+    let Platform = new Sprite(900, 750, 2008, 10, 'k');
     Platform.color = '#d3d3d3';
 
-    //obsticles
-    let obsticles = new Sprite(700, 620, 20, 'k');
-    obsticles.color = 'Red';
-    
-    
-    
+    // Group for coins
+    obsticles_Group = new Group();
 
 
 }
@@ -36,9 +32,23 @@ function setup() {
 function draw() { 
     background('#1e2a47');
     if (mouse.presses()) {
-        Player_Box.vel.y = 50;
+        Player_Box.vel.y = 60;
         console.log("hi")
-        }
+    }
+    
+    //obsticles
+    function spawn_obsticles() {
+        let y = random(640, 730);
+        let obsticles = new Sprite(1900, y , 30, 'k');
+        obsticles.color = 'red';
+        obsticles.vel.x = -2;
+        obsticles_Group.add(obsticles);
+    }
+
+    // Spawn initial coin
+    spawn_obsticles();
+
+
 }
 
 /*******************************************************/
