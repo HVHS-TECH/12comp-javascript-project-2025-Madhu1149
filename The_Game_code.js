bb8b9e459699acd0a91b939902442ac751297187
Madhu1149 <23477mt@hvhs.school.nz>
2025-03-26 20:52:53 +0000

    deleting obsticles after they reach the end of the screen and adding score

--- The_Game_code.js
+++ The_Game_code.js
@@ -2,7 +2,6 @@
 // variables and constants
 /*******************************************************/
 const rotationSpeed = 0;
-const hi = hello
 var score = 0;
 
 
@@ -23,9 +22,15 @@ function setup() {
     const  Platform = new Sprite(900, 750, 2008, 10, 'k');
     Platform.color = '#d3d3d3';
 
+    // end of screen wall
+      wall = new Sprite(10, 450, 8, 800, 'k');
+    wall.color = 'white';
+
     // Group for coins
     obsticles_Group = new Group();
 
+    // Register a callback for collision
+    obsticles_Group.collides(wall,delete_obsticles);
 
 }
 
@@ -50,15 +55,13 @@ function draw() {
         Player_Box.vel.y = 60;
     
     } 
-
-    // Spawn initial obsticles
-    if (random(0,3000)<30){
+     // Spawn initial obsticles
+     if (random(0,3000)<30){
         spawn_obsticles();
     }
 
      // Displaying score 
      displayScore();
-     Add_score()
 
 }
 
@@ -69,11 +72,12 @@ function displayScore(){
     fill('white') 
 	text("Score: " + score, 10,20);
 }
-function Add_score(){
-    if(hi = hello)score=+1;
+// delete obsticles after they go out of screen
+function delete_obsticles(_delete_obsticle, _wall){
+    //delete the obsticle which collides
+    _delete_obsticle.remove();
+    score += 1;
 }
-
-
 
 
 /*******************************************************/
